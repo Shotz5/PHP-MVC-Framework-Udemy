c0584a230f3881064f384b813389d1d4098ff2d2
Alex Kitsul <kitsulalex@gmail.com>
2024-03-24 16:47:28 -0700

    S3.18

--- www/index.php
+++ www/index.php
@@ -3,20 +3,8 @@
 $action = $_GET["action"];
 $controller = $_GET["controller"];
 
-if ($controller === "products") {
+require "src/controllers/$controller.php";
+$controller_object = new $controller;
 
-    require "src/controllers/products.php";
-    $controller_object = new Products;
 
-} elseif ($controller === "home") {
-
-    require "src/controllers/home.php";
-    $controller_object = new Home;
-
-}
-
-if ($action === "index") {
-    $controller_object->index();
-} elseif ($action === "show") {
-    $controller_object->show();
-}
+$controller_object->$action();
