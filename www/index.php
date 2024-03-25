13267e544973f5764f22bd7de39046e005859254
Alex Kitsul <kitsulalex@gmail.com>
2024-03-24 20:13:30 -0700

    S9.65

--- www/index.php
+++ www/index.php
@@ -15,15 +15,5 @@ $router->add('/products', ["controller" => "products", "action" => "index"]);
 $router->add('/', ["controller" => "home", "action" => "index"]);
 $router->add("/{controller}/{action}");
 
-$params = $router->match($path);
-
-if ($params === false) {
-    exit("No route matched");
-}
-
-$action = $params["action"];
-$controller = "App\Controllers\\" . ucwords($params["controller"]);
-
-$controller_object = new $controller;
-
-$controller_object->$action();
+$dispatcher = new Framework\Dispatcher($router);
+$dispatcher->handle($path);
