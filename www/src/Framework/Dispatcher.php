e3e2a91a3e8541ca989a167b75be15e4795f38e6
Alex Kitsul <kitsulalex@gmail.com>
2024-03-24 20:20:10 -0700

    S9.67

--- www/src/Framework/Dispatcher.php
+++ www/src/Framework/Dispatcher.php
@@ -23,19 +23,19 @@ class Dispatcher
 
         $controller_object = new $controller;
 
-        $this->getActionArguments($controller, $action);
+        $this->getActionArguments($controller, $action, $params);
 
         $controller_object->$action();
     }
 
-    private function getActionArguments(string $controller, string $action)
+    private function getActionArguments(string $controller, string $action, array $params)
     {
         $method = new ReflectionMethod($controller, $action);
 
         foreach ($method->getParameters() as $parameter) {
             $name = $parameter->getName();
 
-            echo $name, " ";
+            echo $name, " = " , $params[$name], " ";
         }
     }
 }
