9e4e4ed4599a8aea1bbd97ae9930f6704cf203d3
Alex Kitsul <kitsulalex@gmail.com>
2024-03-24 23:06:44 -0700

    S11.84

--- www/src/App/Models/Product.php
+++ www/src/App/Models/Product.php
@@ -3,17 +3,15 @@
 namespace App\Models;
 
 use PDO;
+use App\Database;
 
 class Product
 {
     public function getData(): array
     {
-        $dsn = "mysql:host=mysql;dbname=product_db;charset=utf8;port=3306";
+        $db = new Database;
 
-        $pdo = new PDO($dsn, "product_db_user", "secret", [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        ]);
-
+        $pdo = $db->getConnection();
         $stmt = $pdo->query("SELECT * FROM product");
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
