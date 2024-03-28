27f565067c433c7a58c94a135d0635d0defbfaa1
Alex Kitsul <kitsulalex@gmail.com>
2024-03-27 19:54:58 -0700

    S14.107

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$show_errors = true;
+$show_errors = false;
 ini_set("display_errors", (int)$show_errors);
 ini_set("log_errors", 1);
 ini_set("error_log", "/var/www/html/logs/error_log");
@@ -19,16 +19,7 @@ if ($path === false) {
     throw new UnexpectedValueException("Malformed url: {$_SERVER["REQUEST_URI"]}");
 }
 
-$router = new Framework\Router;
-
-$router->add("/admin/{controller}/{action}", ["namespace" => "Admin"]);
-$router->add("/{title}/{id:\d+}/{page:\d+}", ["controller" => "products", "action" => "showPage"]);
-$router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
-$router->add("/{controller}/{id:\d+}/{action}");
-$router->add('/home/index', ["controller" => "home", "action" => "index"]);
-$router->add('/products', ["controller" => "products", "action" => "index"]);
-$router->add('/', ["controller" => "home", "action" => "index"]);
-$router->add("/{controller}/{action}");
+$router = require "config/routes.php";
 
 $container = new Framework\Container;
 
